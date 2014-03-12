6ed99d779c631bf86e313965b01c8440a478a33f
Xavier Roche <xavrock.os@gmail.com>
2014-03-12 17:17:09 +0100

    Add Motor features -temporaly commit-
    Signed-off-by: Xavier Roche <xavrock.os@gmail.com>

--- src/drone_motor.c
+++ src/drone_motor.c
@@ -1,18 +1,46 @@
 #include "drone_motor.h"
 
-drone_cmd_motor d_motor_cmd;
+static drone_cmd_motor 	d_motor_cmd;
+static drone_cmd_motor	d_prev_motor_cmd;	
+static drone_quad_motor	d_quad_motor;
 
-
-int init_motor()
+/*
+ * init_motor
+ * 
+ * initialize  motor_current speed to stop value
+ * 
+ */
+int init_motor(drone_motor *motor, int clkw)
 {
+	motor->motor_max 		= MOTOR_MAX;
+	motor->motor_min 		= MOTOR_MIN;
+	motor->motor_stop 		= MOTOR_STOP;
+	motor->motor_current	= MOTOR_STOP;
+	motor->motor_alert		= 0;
 	
+	if( clkw > 1)
+		motor->motor_clockwise 	= 1;
+	else if( clkw < 0 )
+		motor->motor_clockwise 	= 0;
+	else
+		motor->motor_clockwise	= clkw;
+		
 	return 1;
 }
 
-
+/*
+ * init_quad_motor 
+ * 
+ * front_left and back_right motors work clockwise
+ * front_right and back_left both work anti-clockwise
+ * 
+ */
 int init_quad_motor()
 {
-	
+	init_motor( &d_quad_motor.motor_front_l, 1 );
+	init_motor( &d_quad_motor.motor_front_r, 0 );
+	init_motor( &d_quad_motor.motor_back_l, 0 );
+	init_motor( &d_quad_motor.motor_back_r, 1 );
 	return 1;
 }
 
--- src/drone_motor.h
+++ src/drone_motor.h
@@ -1,18 +1,20 @@
 #ifndef DRONE_MOTOR_H
 #define DRONE_MOTOR_H
 
-#define MOTOR_CMD_MAX 	170
-#define MOTOR_CMD_MIN 	64
-#define MOTOR_CMD_STOP 	63
+#include <stdio.h>
+
+#define MOTOR_MAX 	170
+#define MOTOR_MIN 	64
+#define MOTOR_STOP 	63
 
 // Unitary motor description
 typedef struct {
-	int 	motor_cmd_max;
-	int 	motor_cmd_min;
-	int		motor_cmd_stop;
-	int 	motor_cmd_current;
-	bool 	motor_clockwise;
-	bool	motor_alert;
+	int 	motor_max;
+	int 	motor_min;
+	int		motor_stop;
+	int 	motor_current;
+	int 	motor_clockwise;
+	int		motor_alert;
 } drone_motor;
 
 // Drone motor based description
@@ -29,11 +31,11 @@ typedef struct {
 	int		cmd_rot_x;
 	int		cmd_rot_y;
 	int		cmd_rot_z;
-	bool	stop;
+	int		stop;
 } drone_cmd_motor;
 
 
-int init_motor();
+int init_motor(drone_motor *motor, int clkw);
 int init_quad_motor();
 int set_cmd_quad_motor(int c_up, int c_rotx, int c_roty, int c_rotz);
 void interpret_cmd();
