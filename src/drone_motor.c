2d3138c2310f9ebd85d9eaf647f7778174ad9af6
Xavier Roche <xavrock.os@gmail.com>
2014-03-16 15:29:31 +0100

    Adding RPATH config

--- src/drone_motor.c
+++ src/drone_motor.c
@@ -16,6 +16,7 @@ int init_motor(drone_motor *motor, int clkw)
 	motor->m_min 		= MOTOR_MIN;
 	motor->m_stop 		= MOTOR_STOP;
 	motor->m_current	= MOTOR_STOP;
+	motor->m_previous	= MOTOR_STOP;
 	motor->m_alert		= 0;
 	
 	if( clkw > 1)
@@ -25,7 +26,7 @@ int init_motor(drone_motor *motor, int clkw)
 	else
 		motor->m_clockwise	= clkw;
 		
-	return 1;
+	return 0;
 }
 
 /*
@@ -41,14 +42,33 @@ int init_quad_motor()
 	init_motor( &d_quad_motor.m_front_r, 0 );
 	init_motor( &d_quad_motor.m_back_l, 0 );
 	init_motor( &d_quad_motor.m_back_r, 1 );
+	return 0;
+}
+
+
+int	set_motor_pwm(drone_motor *motor, int pwm)
+{
+	if( pwm >= MOTOR_MIN && pwm <= MOTOR_MAX )
+	{
+		motor->m_previous = motor->m_current;
+		motor->m_current = pwm;
+		return 0;
+	}
 	return 1;
 }
 
+int set_motor_stop(drone_motor *motor)
+{
+	motor->m_previous = motor->m_current;
+	motor->m_current = MOTOR_STOP;
+	
+	return 0;
+}
 
 int set_cmd_quad_motor(int c_up, int c_rotx, int c_roty, int c_rotz, int c_stop)
 {
 	
-	return 1;
+	return 0;
 }
 
 
--- src/drone_motor.h
+++ src/drone_motor.h
@@ -13,6 +13,7 @@ typedef struct {
 	int 	m_min;
 	int		m_stop;
 	int 	m_current;
+	int		m_previous;
 	int 	m_clockwise;
 	int		m_alert;
 } drone_motor;
@@ -35,11 +36,13 @@ typedef struct {
 } drone_cmd_motor;
 
 
-int init_motor(drone_motor *motor, int clkw);
-int init_quad_motor();
-int set_cmd_quad_motor(int c_up, int c_rotx, int c_roty, int c_rotz, int c_stop);
-void interpret_cmd();
-void normalize_cmd();
-int through_DC_cmd();
+int 	init_motor(drone_motor *motor, int clkw);
+int		set_motor_pwm(drone_motor *motor, int pwm);
+int		set_motor_stop(drone_motor *motor);
+int 	init_quad_motor();
+int 	set_cmd_quad_motor(int c_up, int c_rotx, int c_roty, int c_rotz, int c_stop);
+void 	interpret_cmd();
+void 	normalize_cmd();
+int 	through_DC_cmd();
 
 #endif /* DRONE_MOTOR_H */
